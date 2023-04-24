36904ca0ab1f6435a1ea8aef9349134cfc00d144
Ahmed Alsaab <ahmed_alsaab@hotmail.com>
2023-04-24 21:21:21 +0100

    update comments in tasks.

--- cypress/e2e/getting-started/tasks.cy.js
+++ cypress/e2e/getting-started/tasks.cy.js
@@ -1,6 +1,6 @@
 describe('Complete the tasks', () => {
   beforeEach(() => {
-    // 1. Create a beforeEach loop that declines cookies
+    // 1. Create a beforeEach that declines cookies
   });
 
   it('Validates each navigation destination', () => {
@@ -10,7 +10,7 @@ describe('Complete the tasks', () => {
 
   it('Validates the search criteria', () => {
     // 3. 1.Create a scenario which performs a search for the word 'node' via the search bar
-    //    2.Intercept the search request
-    //    3.Validate the search request contains the 'node' search criteria by asserting the correct key in the response
+    //    2.Intercept the request which fires of the search request
+    //    3.Validate it was a successful request
   });
 });
